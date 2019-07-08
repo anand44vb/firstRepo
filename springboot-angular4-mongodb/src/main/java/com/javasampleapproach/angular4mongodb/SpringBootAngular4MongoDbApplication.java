40a1c5bb7375b5aba7cd0a50fb50168e8b4b936e
31987 <31987@BLRPRAGWN31987.ITCINFOTECH.com>
2019-07-08 16:59:42 +0530

    Changees in config

--- springboot-angular4-mongodb/src/main/java/com/javasampleapproach/angular4mongodb/SpringBootAngular4MongoDbApplication.java
+++ springboot-angular4-mongodb/src/main/java/com/javasampleapproach/angular4mongodb/SpringBootAngular4MongoDbApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class SpringBootAngular4MongoDbApplication {
 
 	public static void main(String[] args) {
