89914be8f788dee33d38200e802a5e85f2c17770
31987 <31987@BLRPRAGWN31987.ITCINFOTECH.com>
2019-07-21 16:06:50 +0530

    Added autoconfig

--- springboot-angular4-mongodb/src/main/java/com/javasampleapproach/angular4mongodb/SpringBootAngular4MongoDbApplication.java
+++ springboot-angular4-mongodb/src/main/java/com/javasampleapproach/angular4mongodb/SpringBootAngular4MongoDbApplication.java
@@ -1,11 +1,13 @@
 package com.javasampleapproach.angular4mongodb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
+@EnableAutoConfiguration
 public class SpringBootAngular4MongoDbApplication {
 
 	public static void main(String[] args) {
